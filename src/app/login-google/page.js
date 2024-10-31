72d5a5ae0d7b539b7173cedc7f6c0bc166b3c3f7
ABDUL NAVAS CK <cknavas123@gmail.com>
2024-10-31 17:10:57 +0530

    next-auth google login

---
+++ src/app/login-google/page.js
@@ -0,0 +1,13 @@
+import Register from '@/components/RegisterZod';
+import SignIn from '@/components/signIn';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className='form-container p-10'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default page;
